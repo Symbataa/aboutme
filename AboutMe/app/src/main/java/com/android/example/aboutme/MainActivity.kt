ee182df9ee808b07706b196b5a5af9fcc5dd4d69
Symbataa <73997172+Symbataa@users.noreply.github.com>
2022-01-09 10:42:28 +0600

    Add files via upload

---
+++ AboutMe/app/src/main/java/com/android/example/aboutme/MainActivity.kt
@@ -0,0 +1,18 @@
+
+
+package com.android.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/**
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *     * LinearLayout with TextViews, ImageView, and ScrollView
+ *     * ScrollView to display scrollable text
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
